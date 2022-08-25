92d76d216f4e2217a5b3fe45b02e392148362eb7
Antonio Curci <62550982+antoniocurci06@users.noreply.github.com>
2022-08-25 18:45:28 +0200

    update and files creation

---
+++ src/Interpreter.hs
@@ -0,0 +1,3 @@
+-- Antonio Curci
+-- Mat. 761049
+-- CurcIMP, FMCS 2021-2022
---
+++ src/Main.hs
@@ -0,0 +1,13 @@
+-- Antonio Curci
+-- Mat. 761049
+-- CurcIMP, FMCS 2021-2022
+
+module Main where
+import System.IO
+
+main :: IO()
+main = do {
+ putStrLn "CurcIMP: An interpreter for an imperative language";
+ putStrLn "Please select an option from the following to start";
+ mainWhile;
+}
